f76ff815ce114fa9588ee4f05b68532a2bcf2a52
kaifumi <kaifumi1001@gmail.com>
2020-08-12 00:36:39 +0900

    Staffフォルダ追加

---
+++ staff/staff_add.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ろくまる農園</title>
+</head>
+<body>
+    スタッフ追加<br/>
+    <br/>
+    <form method="post" action="staff_add_check.php">
+        スタッフ名を入力してください。<br/>
+        <input type="text" name="name" style="width:200px"><br/>
+        パスワードを入力してください。<br/>
+        <input type="text" name="pass" style="width:100px"><br/>
+        パスワードをもう一度入力してください。<br/>
+        <input type="text" name="pass2" style="width:100px"><br/>
+        <br/>
+        <input type="button" onclick="history.back()" value="戻る">
+        <input type="submit" value="OK">
+    </form>
+</body>
+</html>
