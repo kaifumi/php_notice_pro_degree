7294bdfb7e30e0012f90cd3270dfc53eb65578bd
kaifumi <kaifumi1001@gmail.com>
2020-08-13 00:07:56 +0900

    staff_add_check.phpを追加

---
+++ staff/staff_add_check.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ろくまる農園</title>
+</head>
+<body>
+<?php
+    $staff_name = $_POST['name'];
+    $staff_pass = $_POST['pass'];
+    $staff_pass2 = $_POST['pass2'];
+
+    $staff_name = htmlspecialchars($staff_name, ENT_QUATES, 'UTF-8');
+    $staff_pass = htmlspecialchars($staff_name, ENT_QUATES, 'UTF-8');
+    $staff_pass2 = htmlspecialchars($staff_name, ENT_QUATES, 'UTF-8');
+
+    if($staff_name == '') {
+        print 'スタッフ名が入力されていません。<br/>';
+    } else {
+        print 'スタッフ名：';
+        print $staff_name;
+        print '<br/>';
+    }
+
+    if($staff_pass == '') {
+        print 'パスワードが入力されていません。<br/>';
+    }
+
+    if($staff_pass != staff_pass2) {
+        print 'パスワードが一致しません。<br/>';
+    }
+
+    if ($staff_name = '' || $staff_pass == '' || staff_pass != $staff_pass2) {
+        print '<form>';
+        print '<input type="button" onclick="history.back()" value="戻る">';
+        print '</form>';
+    } else {
+        $staff_pass = md5($staff_pass);
+        print '<form method="post" action="staff_add_done.php">';
+        print '<input type="hidden" name="name" value="'.$staff_name.'">';
+        print '<input type="hidden" name="pass" value="'.$staff_pass.'">';
+        print '<br />';
+        print '<input type="button" onclick="history.back()" value="戻る">';
+        print '<input type="submit" value="OK">';
+        print '</form>';
+    }
+?>
+</body>
+</html>
